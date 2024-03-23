6011c055ecbe857d34eb0510aef6746f88db920a
Seung-su Lee <60792515+siotsiot@users.noreply.github.com>
2024-03-23 20:35:32 +0900

    Update 3.java

--- 3주차/3.java
+++ 3주차/3.java
@@ -2,7 +2,8 @@ class Circle {
 	public int radius;
 	public String color;
 	public int x, y;
-	
+
+	// 생성자를 이용해 필드 초기화
 	public Circle() {
 		radius = 100;
 		color = "빨간색";
@@ -15,44 +16,29 @@ class Circle {
 		radius = 50;
 		color = s;
 	}
-	public Circle(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
 	
+	// move() 메서드를 이용해 원을 좌표를 이동 
+	public void move() {
+		this.x = (int)(Math.random() * 50) + 1;
+		this.y = (int)(Math.random() * 50) + 1;
+	}
 }
 
 public class CircleTest {
 	public static void main(String[] args) {
-		Circle c1, c2, c3, m;
+		Circle c1, c2, c3;
 		c1 = new Circle();
 		c2 = new Circle(10, "파란색");
 		c3 = new Circle("노란색");
-		
-		int move_x, move_y;
-		
+
 		for (int i = 1; i <= 100; i++) {
-			int randomNum = (int)(Math.random() * 3) + 1;
-			switch(randomNum) {
-			case 1:
-				move_x = (int)(Math.random() * 50) + 1;
-				move_y = (int)(Math.random() * 50) + 1;
-				m = new Circle(move_x, move_y);
-				System.out.println(i + ". 원 #1(반지름" + c1.radius + ", 색상: " + c1.color + ")은 (" + m.x + ", " + m.y + "으로 이동하였음)");
-				break;
-				
-			case 2:
-				move_x = (int)(Math.random() * 50) + 1;
-				move_y = (int)(Math.random() * 50) + 1;
-				m = new Circle(move_x, move_y);
-				System.out.println(i + ". 원 #2(반지름" + c2.radius + ", 색상: " + c2.color + ")은 (" + m.x + ", " + m.y + "으로 이동하였음)");
-			
-			case 3:
-				move_x = (int)(Math.random() * 50) + 1;
-				move_y = (int)(Math.random() * 50) + 1;
-				m = new Circle(move_x, move_y);
-				System.out.println(i + ". 원 #3(반지름" + c3.radius + ", 색상: " + c3.color + ")은 (" + m.x + ", " + m.y + "으로 이동하였음)");
-			}
+			System.out.println(i + "번째 이동");
+			c1.move();
+			System.out.printf("원 #1(반지름: %3d, 색상: %s)은 (%2d, %2d으로 이동하였음)\n", c1.radius, c1.color, c1.x, c1.y);
+			c2.move();
+			System.out.printf("원 #2(반지름: %3d, 색상: %s)은 (%2d, %2d으로 이동하였음)\n", c2.radius, c2.color, c2.x, c2.y);
+			c3.move();
+			System.out.printf("원 #3(반지름: %3d, 색상: %s)은 (%2d, %2d으로 이동하였음)\n", c3.radius, c3.color, c3.x, c3.y);
 		}
 	}
 }
