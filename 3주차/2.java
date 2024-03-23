f893fff08c102614065c001e063d930b9393d401
Seung-su Lee <60792515+siotsiot@users.noreply.github.com>
2024-03-23 20:08:54 +0900

    Update 2.java

--- 3주차/2.java
+++ 3주차/2.java
@@ -1,28 +1,24 @@
-// 미완성
 import java.util.Scanner;
 
 class Account {
-	private String ownerName;
-	private double balance = 0;
+	private double balance;
 	
-	// 생성자
-	public Account(String s, int i) {
-		ownerName = s;
-		balance += i;
+	// 생성자로 balance를 초기화
+	public Account() {
+		balance = 0;
 	}
-	
-	// 설정자, 접근자
-	public String getOwnerName() {
-		return ownerName;
-	}
-	public void setOwnerName(String ownerName) {
-		this.ownerName = ownerName;
-	}
-	public double getBalance() {
+	// 접근자를 통해 balance의 값을 출력
+	double getBalance() {
 		return balance;
 	}
-	public void setBalance(double balance) {
-		this.balance = balance;
+	
+	// 설정자를 통해 balance의 값을 변경
+	void deposit(double balance) {
+		this.balance += balance;
+	}
+	
+	void withdraw(double balance) {
+		this.balance -= balance;
 	}
 }
 
@@ -30,6 +26,7 @@ public class AccountTest {
 	public static void main(String[] args) {
 		
 		int menu = 0;
+		Account obj = new Account();
 		
 		do {
 			System.out.println("==== 은행 계좌 시스템 ====");
@@ -43,41 +40,46 @@ public class AccountTest {
 			
 			menu = sc.nextInt();
 			
-			switch(menu){
-				case 1:
-					// 입금
-					System.out.print("누구에게 입금합니까? ");
-					sc.nextLine();
-					String name1 = sc.nextLine();
-					System.out.print("얼마를 입금합니까? ");
-					int money = sc.nextInt();
-					Account obj = new Account(name1, money);
+			switch(menu) {
+				case 1: // 입금
+					int deposit;
+					while(true) {
+						System.out.print("얼마를 입금합니까? ");
+						deposit = sc.nextInt();
+						if (deposit > 0)
+							break;
+						else
+							System.out.println("양수를 입력하세요.");
+					}
+					obj.deposit(deposit);
 					System.out.println("입금이 완료됐습니다.");
-					System.out.println("이름: " + obj.getOwnerName());
 					System.out.println("잔액: " + obj.getBalance());
-					
 					break;
 					
-				case 2:
-					// 출금
-					System.out.print("누구에게 입금합니까? ");
-					sc.nextLine();
-					String name2 = sc.nextLine();
-					System.out.print("얼마를 입금합니까? ");
-					int money2 = sc.nextInt();
-					Account obj2 = new Account(name2, money2);
-					System.out.println("입금이 완료됐습니다.");
-					System.out.println("이름: " + obj2.getOwnerName());
-					System.out.println("잔액: " + obj2.getBalance());
+				case 2: // 출금
+					int withdraw;
+					while(true) {
+						System.out.print("얼마를 출금합니까? ");
+						withdraw = sc.nextInt();
+						if (obj.getBalance() >= withdraw)
+							break;
+						else
+							System.out.println("잔액을 초과해서 입력했습니다.");
+					}
+					obj.withdraw(withdraw);
+					System.out.println("출금이 완료됐습니다.");
+					System.out.println("잔액: " + obj.getBalance());
 					break;
 					
-				case 3:
-					// 잔액 조회
-					
-					
+				case 3: // 잔액 조회
+					System.out.println("잔액: " + obj.getBalance());
+					break;
+				
+				default:
+					System.out.println("번호를 다시 입력해주세요.");
 					break;
 			} 
 		} while (menu != 4);
-		System.out.println("프로그램이 종료되었습니다.");
+		System.out.print("프로그램이 종료되었습니다.");
 	}
 }
